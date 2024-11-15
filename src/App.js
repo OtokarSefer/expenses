92c68b1d1d633cd982e73596d77bbe326a302c4c
otokarsefer <otokar.sefer@voco.ee>
2024-11-15 21:02:53 +0200

    ül.3 tehtud

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import './App.css';
 import ExpenseItem  from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {  
+      date: new Date(2024, 10, 12),
+      title: 'werid aah book',
+      price: 3000.99
+    },
+    {  
+      date: new Date(2024, 10, 12),
+      title: 'pre-shitted pants!',
+      price: 3.99
+    },
+  ]
+
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   )
 }
