dcd2062497c937a4aece50bd0ace9d149f563efe
otokarsefer <otokar.sefer@voco.ee>
2024-11-18 17:36:10 +0200

    doc. 10 halvasti tehtud

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
-import ExpenseDate from './ExpenseDate.js'
 import './ExpenseItem.css'
+import Card from '../UI/card'
+import ExpenseDate from './ExpenseDate'
+import { useState } from 'react'
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
 
+    const clickHandler = () => {
+        console.log('Ouch!');
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+        }
     return (
-        <div className='expense-item'>
-            <ExpenseDate date={props.data.date}/>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.expenseData.date}/>
             <div className='expense-item__description'>
-                <h2>{props.data.title}</h2>
-                <div className='expense-item__price'>{props.data.price}</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Dont Click Me</button>
+        </Card>
     )
 }
 
